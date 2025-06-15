82de2caafca7a2138b838ec655457b5aab786649
Eason Chang <eason@easonchang.com>
2025-06-15 01:43:14 -0600

    Remove redundant autodocs tag

--- src/components/DebugPanel.stories.ts
+++ src/components/DebugPanel.stories.ts
@@ -11,7 +11,6 @@ const meta = {
   parameters: {
     layout: "centered",
   },
-  tags: ["autodocs"],
 } satisfies Meta<typeof DebugPanel>;
 
 export default meta;
--- src/components/PlayerList.stories.ts
+++ src/components/PlayerList.stories.ts
@@ -7,7 +7,6 @@ const meta = {
   parameters: {
     layout: "centered",
   },
-  tags: ["autodocs"],
 } satisfies Meta<typeof PlayerList>;
 
 export default meta;
