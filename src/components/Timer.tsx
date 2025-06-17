6d27f4c3e79f381def9a0e0e7b5bab4780ed389d
Eason Chang <eason@easonchang.com>
2025-06-16 23:44:50 -0600

    Update Timer style

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,5 +1,8 @@
 "use client";
 
+import { Block } from "./Block";
+import { Typography } from "./Typography";
+
 interface TimerProps {
   seconds: number;
 }
@@ -8,8 +11,10 @@ export function Timer({ seconds }: TimerProps) {
   const minutes = Math.floor(seconds / 60);
   const remainingSeconds = seconds % 60;
   return (
-    <div className="text-2xl font-bold text-gray-900">
-      {minutes}:{remainingSeconds.toString().padStart(2, "0")}
-    </div>
+    <Block className="py-2 px-4">
+      <Typography variant="h2">
+        {minutes}:{remainingSeconds.toString().padStart(2, "0")}
+      </Typography>
+    </Block>
   );
 }
