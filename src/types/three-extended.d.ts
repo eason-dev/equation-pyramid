5cb164bae65dd1d2f815081496f74faf9055bb70
carolhsiaoo <zhuyuanxiao0@gmail.com>
2025-06-21 16:19:08 -0600

    Add shader background and Leva controls to AppPage

---
+++ src/types/three-extended.d.ts
@@ -0,0 +1,10 @@
+import type { Object3DNode } from '@react-three/fiber'
+import type { ShaderMaterial } from 'three'
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      trippyMaterial: Object3DNode<ShaderMaterial, typeof ShaderMaterial>
+    }
+  }
+} 
