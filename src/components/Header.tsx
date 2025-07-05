b50f980759adb44b7f858f18a5460dc0fe1c1b2e
Eason Chang <eason@easonchang.com>
2025-07-05 02:32:22 -0600

    Fix header logo button to refresh page when is in index page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,25 @@
 "use client";
 
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useButtonSound } from "@/hooks/useButtonSound";
 
 export function Header() {
   const { playButtonSound } = useButtonSound();
+  const pathname = usePathname();
+
+  const handleLogoClick = (e: React.MouseEvent) => {
+    playButtonSound();
+    if (pathname === "/") {
+      e.preventDefault();
+      window.location.reload();
+    }
+  };
 
   return (
     <header className="flex items-center justify-between p-6">
       {/* Logo */}
-      <Link href="/" onClick={playButtonSound} className="cursor-pointer">
+      <Link href="/" onClick={handleLogoClick} className="cursor-pointer">
         <img src="/logo.svg" alt="Logo" className="h-8 w-auto" />
       </Link>
 
